bad314a3671b7d80a34778f726dceee83dbf18cc
EduardoBarcos <gmatiasa@miumg.edu.gt>
2024-09-28 15:53:03 -0600

    Actualización de los cambios realizados en el proyecto Django

--- Tarea/admin.py
+++ Tarea/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Recurso
 
-# Register your models here.
+@admin.register(Recurso)
+class RecursoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'descripcion', 'propietario')  # Muestra estos campos en la lista
+    search_fields = ('nombre',)  # Permite buscar por nombre
